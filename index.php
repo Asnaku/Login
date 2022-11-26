3730eaa721aab363c01e9f5e23bb0dbc218584f7
Asnaku <asnakuarja2016@gmail.com>
2022-11-26 21:49:34 +0300

    index is modified to link to Book Store

--- index.php
+++ index.php
@@ -1,25 +1,49 @@
-<?php 
+<?php
 session_start();
 
-	include("connection.php");
-	include("functions.php");
+include("connection.php");
+include("functions.php");
 
-	$user_data = check_login($con);
+$user_data = check_login($con);
 
 ?>
 
+
+
 <!DOCTYPE html>
 <html>
+
 <head>
+	<style>
+		h1 {
+			text-align: center;
+		}
+
+		p {
+			text-align: center;
+		}
+
+		div {
+			text-align: center;
+		}
+	</style>
 	<title>My website</title>
 </head>
+
 <body>
+	<h1>Hello, <?php echo $user_data['user_name']; ?></h1>
 
-	<a href="logout.php">Logout</a>
-	<h1>This is the index page</h1>
-
+	<p>
+		<em>
+			<a href="http://localhost/Book%20Store/CRUD/index.php">Click to open Book Store</a>
+		</em>
+	</p>
 	<br>
-	Hello, <?php echo $user_data['user_name']; ?>
+	<p>
+		<em>
+			<a href="logout.php">Logout</a>
+		</em>
+	</p>
 </body>
-</html>
 
+</html>
